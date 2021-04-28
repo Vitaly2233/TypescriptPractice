0138dbe0ec4357b61529557222d70c9c103aaab2
Vitaly_R <69852955+Vitaly2233@users.noreply.github.com>
2021-04-28 19:21:01 +0300

    Worked with generics in typescript

--- dist/app.js
+++ dist/app.js
@@ -6,3 +6,19 @@ function getLength(value) {
     };
 }
 console.log(getLength("dfsdf"));
+class objectCollection {
+    constructor() {
+        this._objects = [];
+    }
+    addObject(obj) {
+        this._objects.push(obj);
+    }
+    seeAllObjects() {
+        return this._objects;
+    }
+}
+const obj = new objectCollection();
+console.log(obj.seeAllObjects());
+obj.addObject({ name: "vitaly" });
+obj.addObject({ age: 17 });
+console.log(obj.seeAllObjects());
--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,31 @@
 interface TLength {
     length: number
 }
-function getLength<T extends TLength>(value: T): {value: T, count: string} {
+
+function getLength<T extends TLength>(value: T): { value: T, count: string } {
     return {
         value,
-        count : `Object has ${value.length} symbols`
+        count: `Object has ${value.length} symbols`
     }
 }
 
 console.log(getLength("dfsdf"));
+
+
+class objectCollection<T extends object> {
+    private _objects: object[] = [];
+
+    addObject(obj: object) {
+        this._objects.push(obj);
+    }
+    seeAllObjects() {
+        return this._objects;
+    }
+
+}
+
+const obj = new objectCollection();
+console.log(obj.seeAllObjects())
+obj.addObject({name: "vitaly"});
+obj.addObject({age: 17});
+console.log(obj.seeAllObjects())
