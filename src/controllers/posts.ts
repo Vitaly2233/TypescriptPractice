26c8071b958e41d048712fa8475e54662dc2ab27
Vitaly_R <69852955+Vitaly2233@users.noreply.github.com>
2021-05-04 10:38:09 +0300

    small bug fixes with typescript types

--- src/controllers/posts.ts
+++ src/controllers/posts.ts
@@ -2,14 +2,14 @@ import {User, IUser} from "../models/newUser";
 import express, {NextFunction, Request, Response} from "express";
 import {IPost, Post} from "../models/post";
 
-export const seePosts = async (req: Request, res: Response): Promise<void> => {
+export const seePosts = async (req: Request, res: Response): Promise<Response> => {
   const {postName, description} = req.body;
   const username = req.user.username;
   const myPosts:IPost[] = await Post.find({user: username});
-  res.json(myPosts)
+  return res.json(myPosts)
 }
 
-export const addPost = async (req: Request, res: Response, next: NextFunction): Promise<any> => {
+export const addPost = async (req: Request, res: Response, next: NextFunction): Promise<Response> => {
   const {postName, description} = req.body;
   const username = req.user.username;
 
